a0e9cdef2ecbdd14aa430b835a0305d397e44021
mo-hasan92 <102214961+mo-hasan92@users.noreply.github.com>
2024-10-16 20:20:20 +0200

    t

---
+++ A2/A2/src/app/autos/autos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autos',
+  standalone: true,
+  imports: [],
+  templateUrl: './autos.component.html',
+  styleUrl: './autos.component.css'
+})
+export class AutosComponent {
+
+}
